183dc2d4368012b0d423785f404607c59596d79c
Aleksandra Matysiak <almin2526@gmail.com>
2017-07-09 16:35:50 +0200

    Add project files.

---
+++ DaneUczestnicy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RejestrSzkolen
+{
+    public class DaneUczestnicy
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public double PESEL { get; set; }
+    }
+}
